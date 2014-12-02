0cce8022c51fc1567e8f4f33dfdf08d0fa9805a7
Tom Ridge <tom.j.ridge@googlemail.co.uk>
2014-12-02 18:20:24 +0000

    +

--- src/p1_gen_shared.ml
+++ src/p1_gen_shared.ml
@@ -22,12 +22,13 @@ let nonterm_to_parse_fun_name = fun nt -> "parse_"^nt
 (* assume terms are like "1" or ?xyz? for now *)
 let term_to_ocaml = (fun x0 -> 
     let l0 = String.length x0 in
+    let f1 x0 = ("(a \""^(String.sub x0 1 (l0-2) |> String.escaped )^"\")") in
     match l0 <= 1 with
     | true -> (failwith "term_to_ocaml: t0x")
     | false -> (
         match x0.[0],x0.[l0-1] with
-        | '"','"' -> ("(a "^x0^")")
-        | '\'','\'' -> ("(a "^x0^")")
+        | '"','"' -> (f1 x0)
+        | '\'','\'' -> (f1 x0)
         | '?','?' -> ("("^(String.sub x0 1 (l0-2))^")")
         | _ -> (failwith "term_to_ocaml: fjc")))        
 
